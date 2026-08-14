a9ccd716d61e0e31b948a20048ca0c1c5bad42cb
Alisa Sireneva <me@purplesyringa.moe>
2026-08-14 13:23:20 +0300

    Make StaticRef Send + Sync

--- src/static_ref.rs
+++ src/static_ref.rs
@@ -95,6 +95,9 @@ pub struct StaticRef<T: 'static> {
     ptr: RelocatablePtr<T>,
 }
 
+unsafe impl<T: Sync> Send for StaticRef<T> {}
+unsafe impl<T: Sync> Sync for StaticRef<T> {}
+
 // Implement Clone/Copy even for T: !Clone/Copy
 impl<T> Clone for StaticRef<T> {
     fn clone(&self) -> Self {
