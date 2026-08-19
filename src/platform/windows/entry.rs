940d673f8333da7ac500aa3f6c907e1c89909d2c
Alisa Sireneva <me@purplesyringa.moe>
2026-08-19 21:19:08 +0300

    Run WSAStartup in entry

--- src/platform/windows/entry.rs
+++ src/platform/windows/entry.rs
@@ -3,6 +3,7 @@ use crate::{
     subprocess::load_init_handles,
 };
 use std::os::windows::io::{AsRawSocket, FromRawSocket};
+use windows::Win32::Networking::WinSock;
 
 // XXX: very hacky
 struct FakeDeserializer(Deserializer);
@@ -19,6 +20,14 @@ unsafe impl Object for FakeDeserializer {
 }
 
 pub(crate) fn crossmist_main(_args: std::env::Args) -> ! {
+    let mut data = WinSock::WSADATA::default();
+    if unsafe { WinSock::WSAStartup(0x0202, &raw mut data) } != 0 {
+        panic!(
+            "Failed to initialize WinSock: {:?}",
+            std::io::Error::from_raw_os_error(unsafe { WinSock::WSAGetLastError() }.0)
+        );
+    }
+
     let socket = unsafe { load_init_handles() };
 
     // Notify the parent that they can stop keeping the handles alive
